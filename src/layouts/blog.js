8eeb688b66fbc1367d768ba9d718fe19937e7e9b
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 08:45:11 +0000

    blog.js: changed

--- src/layouts/blog.js
+++ src/layouts/blog.js
@@ -29,7 +29,6 @@ export default class Blog extends React.Component {
                     <Link className="post-thumbnail" href={withPrefix(_.get(post, '__metadata.urlPath', null))}><img src={withPrefix(_.get(post, 'frontmatter.thumb_image', null))} alt={_.get(this.props, 'page.frontmatter.thumb_image_alt', null)} /></Link>
                     )}
                     <header className="post-header">
-                      
                       <h2 className="post-title line-left"><Link href={withPrefix(_.get(post, '__metadata.urlPath', null))} rel="bookmark">{_.get(post, 'frontmatter.title', null)}</Link></h2>
                     </header>
                     {_.get(post, 'frontmatter.excerpt', null) && (<React.Fragment>
